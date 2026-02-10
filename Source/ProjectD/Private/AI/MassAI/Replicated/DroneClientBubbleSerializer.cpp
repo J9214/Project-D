d99f821ae759069d912b7a145e92ec44e6861cca
Hyun <hnjogo@gmail.com>
2026-02-10 20:31:15 +0900

    [Add] Drone 용 ClientBubble Serializer 클래스 제작 - FastArray(Item 배열) 과 Handler를 묶고 Delta Serialize를 세팅하는 클래스

---
+++ Source/ProjectD/Private/AI/MassAI/Replicated/DroneClientBubbleSerializer.cpp
@@ -0,0 +1,16 @@
+#include "AI/MassAI/Replicated/DroneClientBubbleSerializer.h"
+#include "Net/Serialization/FastArraySerializer.h"
+
+FDroneClientBubbleSerializer::FDroneClientBubbleSerializer()
+	: FMassClientBubbleSerializerBase()
+	, Agents()
+	, Bubble()
+{
+	Bubble.Initialize(Agents, *this);
+	SetClientHandler(Bubble);
+}
+
+bool FDroneClientBubbleSerializer::NetDeltaSerialize(FNetDeltaSerializeInfo& DeltaParams)
+{
+	return FFastArraySerializer::FastArrayDeltaSerialize<FDroneFastArrayItem, FDroneClientBubbleSerializer>(Agents, DeltaParams, *this);
+}
