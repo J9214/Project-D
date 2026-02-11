8177c19968964f9b90adcf478c57d3eaa8297fc5
LEE MINGU <qazzaq1541@gmail.com>
2026-02-11 16:47:27 +0900

    [feat] Add aim crosshair functionality and weapon change handling

--- Source/ProjectD/Private/Controller/PDPlayerController.cpp
+++ Source/ProjectD/Private/Controller/PDPlayerController.cpp
@@ -1,2 +1,181 @@
 #include "Controller/PDPlayerController.h"
+#include "Blueprint/UserWidget.h"
+#include "Components/Combat/WeaponManageComponent.h"
+#include "DataAssets/Weapon/DataAsset_Weapon.h"
+#include "Weapon/PDWeaponBase.h"
+#include "Pawn/PDPawnBase.h"
 
+void APDPlayerController::BeginPlay()
+{
+	Super::BeginPlay();
+}
+
+void APDPlayerController::SetupInputComponent()
+{
+	Super::SetupInputComponent();
+}
+
+void APDPlayerController::SetPawn(APawn* InPawn)
+{
+	Super::SetPawn(InPawn);
+	
+	if (IsLocalController())
+	{
+		BindWeaponChangedDelegate();
+	}
+}
+
+void APDPlayerController::OnUnPossess()
+{
+	if (IsLocalController())
+	{
+		UnbindWeaponChangedDelegate();
+	}
+	
+	Super::OnUnPossess();
+}
+
+void APDPlayerController::ShowAimCrosshair(TSubclassOf<UUserWidget> CrosshairClass)
+{
+	if (!IsLocalController())
+	{
+		return;
+	}
+	
+	if (CurrentCrosshairWidget && CurrentCrosshairClass == CrosshairClass)
+	{
+		return;
+	}
+
+	if (CurrentCrosshairWidget)
+	{
+		CurrentCrosshairWidget->RemoveFromParent();
+		CurrentCrosshairWidget = nullptr;
+		CurrentCrosshairClass = nullptr;
+	}
+
+	if (!CrosshairClass)
+	{
+		return;
+	}
+	
+	CurrentCrosshairWidget = CreateWidget<UUserWidget>(this, CrosshairClass);
+	if (CurrentCrosshairWidget)
+	{
+		CurrentCrosshairClass = CrosshairClass;
+		CurrentCrosshairWidget->AddToViewport(50);
+	}
+}
+
+void APDPlayerController::HideAimCrosshair()
+{
+	if (!IsLocalController())
+	{
+		return;
+	}
+	
+	if (CurrentCrosshairWidget)
+	{
+		CurrentCrosshairWidget->RemoveFromParent();
+		CurrentCrosshairWidget = nullptr;
+		CurrentCrosshairClass = nullptr;
+	}
+}
+
+void APDPlayerController::OnAimStarted()
+{
+	bWantsToAim = true;
+	ShowAimCrosshair(GetCurrentAimCrosshairClass());
+}
+
+void APDPlayerController::OnAimEnded()
+{
+	bWantsToAim = false;
+	HideAimCrosshair();
+}
+
+TSubclassOf<UUserWidget> APDPlayerController::GetCurrentAimCrosshairClass() const
+{
+	const APDPawnBase* OwnerPawn = Cast<APDPawnBase>(GetPawn());
+	if (!OwnerPawn)
+	{
+		return nullptr;
+	}
+	
+	const UWeaponManageComponent* WeaponComp = OwnerPawn->GetWeaponManageComponent();
+	if (!WeaponComp)
+	{
+		return nullptr;
+	}
+	
+	const APDWeaponBase* Weapon = WeaponComp->GetEquippedWeapon();
+	if (!Weapon || !Weapon->WeaponData)
+	{
+		return nullptr;
+	}
+	
+	return Weapon->WeaponData->AimCrosshairWidgetClass;
+}
+
+void APDPlayerController::HandleWeaponChanged(APDWeaponBase* NewWeapon)
+{
+	if (!IsLocalController())
+	{
+		return;
+	}
+
+	if (!bWantsToAim || !NewWeapon)
+	{
+		HideAimCrosshair();
+		return;
+	}
+
+	if (UDataAsset_Weapon* NewWeaponDA = NewWeapon->WeaponData)
+	{
+		if (TSubclassOf<UUserWidget> Crosshair = NewWeaponDA->AimCrosshairWidgetClass)
+		{
+			ShowAimCrosshair(Crosshair);
+		}
+	}
+}
+
+void APDPlayerController::BindWeaponChangedDelegate()
+{
+	UnbindWeaponChangedDelegate();
+
+	APDPawnBase* OwnerPawn = Cast<APDPawnBase>(GetPawn());
+	if (!OwnerPawn)
+	{
+		return;
+	}
+
+	UWeaponManageComponent* WeaponComp = OwnerPawn->GetWeaponManageComponent();
+	if (!WeaponComp)
+	{
+		return;
+	}
+
+	WeaponComp->OnEquippedWeaponDataChanged.AddUObject(this, &APDPlayerController::HandleWeaponChanged);
+
+	if (bWantsToAim)
+	{
+		ShowAimCrosshair(GetCurrentAimCrosshairClass());
+	}
+}
+
+void APDPlayerController::UnbindWeaponChangedDelegate() const
+{
+	APDPawnBase* OwnerPawn = Cast<APDPawnBase>(GetPawn());
+	if (!OwnerPawn)
+	{
+		return;
+	}
+
+	UWeaponManageComponent* WeaponComp = OwnerPawn->GetWeaponManageComponent();
+	if (!WeaponComp)
+	{
+		return;
+	}
+
+	WeaponComp->OnEquippedWeaponDataChanged.RemoveAll(this);
+}
--- Source/ProjectD/Public/Controller/PDPlayerController.h
+++ Source/ProjectD/Public/Controller/PDPlayerController.h
@@ -4,9 +4,45 @@
 #include "GameFramework/PlayerController.h"
 #include "PDPlayerController.generated.h"
 
+class APDWeaponBase;
+class UUserWidget;
+class UWeaponManageComponent;
+
 UCLASS()
 class PROJECTD_API APDPlayerController : public APlayerController
 {
 	GENERATED_BODY()
 	
+public:
+	virtual void BeginPlay() override;
+	virtual void SetupInputComponent() override;
+
+	virtual void SetPawn(APawn* InPawn) override;
+	virtual void OnUnPossess() override;
+	
+private:
+	UFUNCTION(BlueprintCallable)
+	void OnAimStarted();
+
+	UFUNCTION(BlueprintCallable)
+	void OnAimEnded();
+	
+	void ShowAimCrosshair(TSubclassOf<UUserWidget> CrosshairClass);
+	void HideAimCrosshair();
+
+	TSubclassOf<UUserWidget> GetCurrentAimCrosshairClass() const;
+
+	void HandleWeaponChanged(APDWeaponBase* NewWeapon);
+	
+	void BindWeaponChangedDelegate();
+	void UnbindWeaponChangedDelegate() const;
+	
+private:
+	UPROPERTY()
+	TSubclassOf<UUserWidget> CurrentCrosshairClass;
+	
+	UPROPERTY()
+	TObjectPtr<UUserWidget> CurrentCrosshairWidget;
+	
+	bool bWantsToAim = false;
 };
