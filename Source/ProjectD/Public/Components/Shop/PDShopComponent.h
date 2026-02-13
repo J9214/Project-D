1563b743938ab1d691dcd5a4ad178b68c1d0f12d
KiSungKim23 <essuttr3@gmail.com>
2026-02-13 16:15:36 +0900

    [Add] 상점 Component만 제작

---
+++ Source/ProjectD/Public/Components/Shop/PDShopComponent.h
@@ -0,0 +1,27 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/ActorComponent.h"
+#include "PDShopComponent.generated.h"
+
+
+UCLASS( ClassGroup=(Custom), meta=(BlueprintSpawnableComponent) )
+class PROJECTD_API UPDShopComponent : public UActorComponent
+{
+	GENERATED_BODY()
+
+public:	
+	UPDShopComponent();
+
+	//UFUNCTION(BlueprintCallable, Category = "Shop|Data")
+	//UDataTable* GetItemDataTable() const;
+
+protected:
+
+	//UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Shop|Data")
+	//TSoftObjectPtr<UDataTable> ItemDataTable;
+
+
+};
