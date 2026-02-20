23fd4e04eb09aa22650311835436cbe2f68c7c8f
Hyun <hnjogo@gmail.com>
2026-02-20 17:32:51 +0900

    [Add] Entity 제거용의 신규 Processor 클래스

---
+++ Source/ProjectD/Private/AI/MassAI/Replicated/MassDelayRemovalProcessor.cpp
@@ -0,0 +1,67 @@
+#include "AI/MassAI/Replicated/MassDelayRemovalProcessor.h"
+#include "AI/MassAI/Replicated/DeathScheduleFragment.h"
+#include "AI/MassAI/Replicated/MassEntityTags.h"
+#include "MassExecutionContext.h"
+#include "ProjectD/ProjectD.h"
+#include "Engine/World.h"
+
+#define DLOG(Format, ...) UE_LOG(LogProjectD, Warning, TEXT("[F=%u] " Format), (uint32)GFrameCounter, ##__VA_ARGS__)
+
+UMassDelayRemovalProcessor::UMassDelayRemovalProcessor()
+	:EntityQuery(*this)
+{
+	ExecutionFlags = (int32)EProcessorExecutionFlags::Server;
+	bAutoRegisterWithProcessingPhases = true;
+	bRequiresGameThreadExecution = true;
+	ProcessingPhase = EMassProcessingPhase::PostPhysics;
+}
+
+void UMassDelayRemovalProcessor::ConfigureQueries(const TSharedRef<FMassEntityManager>& EntityManager)
+{
+	EntityQuery.AddRequirement<FDeathScheduleFragment>(EMassFragmentAccess::ReadWrite);
+	EntityQuery.AddTagRequirement<FMassEntityDyingTag>(EMassFragmentPresence::All);
+
+	EntityQuery.RegisterWithProcessor(*this);
+}
+
+void UMassDelayRemovalProcessor::Execute(FMassEntityManager& EntityManager, FMassExecutionContext& Context)
+{
+	const uint32 NowFrame = (uint32)GFrameCounter;
+
+	EntityQuery.ForEachEntityChunk(Context,
+		[this, NowFrame](FMassExecutionContext& ExecContext)
+		{
+			TArrayView<FDeathScheduleFragment> Schedules = ExecContext.GetMutableFragmentView<FDeathScheduleFragment>();
+
+			const int32 NumEntities = ExecContext.GetNumEntities();
+			for (int32 i = 0; i < NumEntities; ++i)
+			{
+				FDeathScheduleFragment& S = Schedules[i];
+				const FMassEntityHandle Entity = ExecContext.GetEntity(i);
+
+				if ((S.bMarkedForRemoval == false) &&
+					(S.RemoveAtFrame != 0) &&
+					(NowFrame >= S.RemoveAtFrame))
+				{
+					S.bMarkedForRemoval = true;
+					ExecContext.Defer().AddTag<FMassEntityPendingRemovalTag>(Entity);
+
+					DLOG("[DelayedRemove] Entity=%d RemoveAt=%u Now=%u",
+						Entity.Index,
+						S.RemoveAtFrame,
+						NowFrame);
+				}
+
+				if ((S.DestroyAtFrame != 0) &&
+					(NowFrame >= S.DestroyAtFrame))
+				{
+					ExecContext.Defer().DestroyEntity(Entity);
+
+					DLOG("[DelayedDestroy] Entity=%d DestroyAt=%u Now=%u",
+						Entity.Index,
+						S.DestroyAtFrame,
+						NowFrame);
+				}
+			}
+		});
+}
