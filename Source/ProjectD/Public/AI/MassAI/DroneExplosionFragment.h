5cf52e393116cb2d754b7f6a9a6299bb80b1b59b
Hyun <hnjogo@gmail.com>
2026-03-03 20:33:27 +0900

    [Add] Drone 폭발용 Fragment 분리

---
+++ Source/ProjectD/Public/AI/MassAI/DroneExplosionFragment.h
@@ -0,0 +1,24 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "MassEntityTypes.h"
+#include "MassEntityHandle.h"
+#include "DroneExplosionFragment.generated.h"
+
+USTRUCT()
+struct PROJECTD_API FDroneExplosionFragment : public FMassSharedFragment
+{
+	GENERATED_BODY()
+public:
+    UPROPERTY(EditAnywhere)
+    float PlayerExplodeRange = 125.0f;
+
+    UPROPERTY(EditAnywhere)
+    float ObstacleRange = 25.0f;
+
+    UPROPERTY(EditAnywhere)
+    float ExplosionRadius = 300.0f;
+
+    UPROPERTY(EditAnywhere)
+    float Damage = 50.0f;
+};
