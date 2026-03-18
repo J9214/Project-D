27b6d9c9723271606206654347504eb3c3cc425f
Hyun <hnjogo@gmail.com>
2026-03-18 20:14:10 +0900

    [Fix] ExplodeProcesser 에서 체력과 Velocity Fragment를 수정하도록 코드 수정

--- Source/ProjectD/Private/AI/MassAI/DroneAllExplodeProcessor.cpp
+++ Source/ProjectD/Private/AI/MassAI/DroneAllExplodeProcessor.cpp
@@ -1,7 +1,10 @@
 #include "AI/MassAI/DroneAllExplodeProcessor.h"
 #include "AI/MassAI/CheckDroneAllExplodeSubsystem.h"
 #include "AI/MassAI/Replicated/MassEntityTags.h"
+#include "AI/MassAI/MassBoidsHealthFragment.h"
 #include "MassExecutionContext.h"
+#include "MassMovementFragments.h"
+#include "ProjectD/ProjectD.h"
 
 UDroneAllExplodeProcessor::UDroneAllExplodeProcessor()
 	: EntityQuery(*this)
@@ -13,8 +16,12 @@ UDroneAllExplodeProcessor::UDroneAllExplodeProcessor()
 
 void UDroneAllExplodeProcessor::ConfigureQueries(const TSharedRef<FMassEntityManager>& EntityManager)
 {
+	EntityQuery.AddRequirement<FMassVelocityFragment>(EMassFragmentAccess::ReadWrite);
+	EntityQuery.AddRequirement<FMassBoidsHealthFragment>(EMassFragmentAccess::ReadWrite);
+
 	EntityQuery.AddTagRequirement<FMassEntitySuicideTag>(EMassFragmentPresence::None);
 	EntityQuery.AddTagRequirement<FMassEntityDyingTag>(EMassFragmentPresence::None);
+	EntityQuery.AddTagRequirement<FMassEntityPendingRemovalTag>(EMassFragmentPresence::None);
 
 	EntityQuery.RegisterWithProcessor(*this);
 }
@@ -38,12 +45,28 @@ void UDroneAllExplodeProcessor::Execute(FMassEntityManager& EntityManager, FMass
 		return;
 	}
 
-	EntityQuery.ForEachEntityChunk(EntityManager, Context, [](FMassExecutionContext& ExecContext)
+	UE_LOG(LogProjectD, Warning, TEXT("[UDroneAllExplodeProcessor] Drone Explose!"));
+
+	EntityQuery.ForEachEntityChunk(Context, [this](FMassExecutionContext& ExecContext)
 		{
+			TArrayView<FMassVelocityFragment> Velocities = ExecContext.GetMutableFragmentView<FMassVelocityFragment>();
+			TArrayView<FMassBoidsHealthFragment> HealthInfos = ExecContext.GetMutableFragmentView<FMassBoidsHealthFragment>();
+
 			const int32 NumEntities = ExecContext.GetNumEntities();
 
 			for (int32 i = 0; i < NumEntities; ++i)
 			{
+				FVector& Velocity = Velocities[i].Value;
+				FMassBoidsHealthFragment& HealthInfo = HealthInfos[i];
+
+				if (HealthInfo.Health <= 0)
+				{
+					continue;
+				}
+
+				HealthInfo.Health = 0.0f;
+				Velocity = FVector::ZeroVector;
+
 				const FMassEntityHandle Entity = ExecContext.GetEntity(i);
 				ExecContext.Defer().AddTag<FMassEntitySuicideTag>(Entity);
 			}
