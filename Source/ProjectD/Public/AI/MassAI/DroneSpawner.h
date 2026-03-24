088c82ce3a3cb56160cd8121f7f2f30755671cee
Hyun <hnjogo@gmail.com>
2026-03-24 16:02:46 +0900

    [Fix] Drone 생성을 일정 프레임으로 나누어 Spawn 시키도록 수정

--- Source/ProjectD/Public/AI/MassAI/DroneSpawner.h
+++ Source/ProjectD/Public/AI/MassAI/DroneSpawner.h
@@ -11,7 +11,31 @@ class PROJECTD_API ADroneSpawner : public AMassSpawner
 
 public:
 	ADroneSpawner();
-	
+
 public:
 	void SpawnDrones();
+
+protected:
+	void ProcessNextSpawnBatch();
+	void ScheduleNextSpawnBatch();
+	void FinishBatchSpawn();
+
+protected:
+	FORCEINLINE bool IsBatchSpawnActive() const { return RemainingBatchCount > 0; }
+
+protected:
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Drone Spawn", meta = (ClampMin = "1"))
+	int32 SpawnOverBatches = 4;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Drone Spawn", meta = (ClampMin = "0.0"))
+	float SpawnBatchInterval = 0.05f;
+
+protected:
+	FTimerHandle SpawnBatchTimerHandle;
+
+	int32 InitialTotalSpawnCount = 0;
+	int32 RemainingSpawnCount = 0;
+	int32 RemainingBatchCount = 0;
+
+	float OriginalSpawnScale = 1.0f;
 };
